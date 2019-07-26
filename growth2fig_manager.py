c9ce81a2dab97dfe933118cb15d14de8c93a7453
Hai He <He@mpimp-golm.mpg.de>
2019-07-26 13:28:37 +0200

    Added instruction and two new funcitons: veiwPlate and viewWells. Commented commands lines for better understanding. Ready to be published.

--- growth2fig_manager.py
+++ growth2fig_manager.py
@@ -1,61 +1,83 @@
 #%%
 # -*- coding: utf-8 -*-
+"""
+@Author: Hai
+The script works on Python 3, you may use VS code (higher than 1.35) in the interactive mode 
+or iPython IDE, such as Spyder.
+The script requires pandas, matplotlib, numpy, scipy and the alonged script 'growth2fig_plotting.py' to work.
+The current verison of scripts will give you some warns, you may ignore them.
+
+Your data should be prepared in excel first, in the format like the test data set.
+It has wells in columns, and time in rows. Data from Biotech machine is perfect fine. 
+Please transpose data from Tecan.
+"""
+
+from IPython import get_ipython
+get_ipython().magic('reset -sf')  # equal to clear in matlab
+get_ipython().magic('matplotlib qt')
+
 import pandas as pd
 from matplotlib import pyplot as plt
 import growth2fig_plotting as g2plot
 
-plt.close('all')
-experiment = pd.DataFrame()
+plt.close('all')  # close all graph windows
+experiment = pd.DataFrame() # DO NOT modify this.
 
-DataFileName = '20190117_ltaE sdaA promoter.xlsx'  # data need to be prepared first in excel
+DataFileName = 'testingData.xlsx'  # data need to be prepared first in excel, and put it into the same folder.
 inc_OD = 0.02  # the starting OD in cuvette unit
 Figure_Type = 'all'
 # Figure_Type = 'mean'
-#Yscale = 'log'
+# Yscale = 'log'
 Yscale = 'linear'
-Xmax = 120  # None or setting with number
-Ymax = 2.8  # None or setting with number
+Xmax = None  # None or setting with number, in hours
+Ymax = None  # None or setting with number, OD
 WIN = 5  # in hours, period in which growth rate is measured
-TH = 0.05  # 
-savefig = 'no'   # eps, png, or no
+TH = 0.05  # Maximum OD below this value is NO GROWTH
+savefig = 'no'   # eps, png, or no to save graphs as .eps or .png files named by graph titles.
 
-# set titles 'Keio $\mathit{\Delta purU}$, 10mM glucose'
-experiment.loc[1,1] = 'fC1SAUX ASS-ltaE-CgadhA \n10mM glc, 10mM Gly, 0.5uM Mn'
-experiment.loc[1,2] = 'fC1Sac PltaES ASS-CgadhA \n10mM glc, 10mM Gly, 0.5uM Mn'
-experiment.loc[1,3] = 'fC1Sac ASS-ltaE-CgadhA \n10mM glc, 10mM Gly, 0.5uM Mn'
-experiment.loc[1,4] = 'fC1Sac ASS-CgadhA-ltaE \n10mM glc, 10mM Gly, 0.5uM Mn'
-experiment.loc[1,5] = '$\mathit{\Delta frm \Delta glyA \Delta serA}$ ASS-ltaE-CgadhA \n10mM glc, 10mM Gly, 0.5uM Mn'
-experiment.loc[1,6] = 'fC1Sac sdaA P_wt \n0.5uM Mn'
-experiment.loc[1,7] = 'fC1Sac PsdaAS \n0.5uM Mn'
-experiment.loc[1,8] = 'fC1Sac ASS-sdaA \n0.5uM Mn'
+# set titles, 'Title'
+experiment.loc[1,1] = 'title 1'
+experiment.loc[1,2] = 'title 2'
+experiment.loc[1,3] = 'title 3'
+experiment.loc[1,4] = 'title 4'
+experiment.loc[1,5] = 'title 5'
+experiment.loc[1,6] = 'title 6'
+experiment.loc[1,7] = 'title 7'
+experiment.loc[1,8] = 'title 8'
+experiment.loc[1,9] = 'title 9'
+experiment.loc[1,10] = 'title 10'
 
-# set replicates
-experiment.loc[2,1] = [['A1','A2'], ['A3','A4'],['A5','A6'],['A7','A8'],['A9','A10'],['A11','A12']]
-experiment.loc[2,2] = [['B1','B2'], ['B3','B4'],['B5','B6'],['B7','B8'],['B9','B10'],['B11','B12']]
-experiment.loc[2,3] = [['C1','C2'], ['C3','C4'],['C5','C6'],['C7','C8'],['C9','C10'],['C11','C12']]
-experiment.loc[2,4] = [['D1','D2'], ['D3','D4'],['D5','D6'],['D7','D8'],['D9','D10'],['D11','D12']]
-experiment.loc[2,5] = [['E1','E2'], ['E3','E4'],['E5','E6'],['E7','E8'],['E9','E10'],['E11','E12']]
-experiment.loc[2,6] = [['F1','F2'], ['F3','F4'],['F5','F6'],['F7','F8'],['F9','F10'],['F11','F12']]
-experiment.loc[2,7] = [['G1','G2'], ['G3', 'G4'], ['G5', 'G6'], ['G7', 'G8'], ['G9', 'G10'], ['G11', 'G12']]
-experiment.loc[2,8] = [['H1','H2'], ['H3', 'H4'], ['H5', 'H6'], ['H7', 'H8'], ['H9', 'H10'], ['H11', 'H12']]
+# set replicates; ['A1','A2']
+experiment.loc[2,1] = [['A1','A2'], ['A3','A4'],['A5','A6']]
+experiment.loc[2,2] = [['B1','B2'], ['B3','B4'],['B5','B6']]
+experiment.loc[2,3] = [['C1','C2'], ['C3','C4'],['C5','C6']]
+experiment.loc[2,4] = [['D1','D2'], ['D3','D4'],['D5','D6']]
+experiment.loc[2,5] = [['E1','E2'], ['E3','E4'],['E5','E6']]
+experiment.loc[2,6] = [['F1','F2'], ['F3','F4'],['F5','F6']]
+experiment.loc[2,7] = [['G1','G2'], ['G3', 'G4'], ['G5', 'G6']]
+experiment.loc[2,8] = [['A7','A8'], ['B7', 'B8'], ['C7', 'C8'], ['D7', 'D8'], ['E7', 'E8'], ['F7', 'F8']]
+experiment.loc[2,9] = [['A9','A10'], ['B9', 'B10'], ['C9', 'C10'], ['D9', 'D10'], ['E9', 'E10'], ['F9', 'F10']]
+experiment.loc[2,10] = [['A11','A12'], ['B11', 'B12'], ['C11', 'C12'], ['D11', 'D12'], ['E11', 'E12'], ['F11', 'F12']]
 
 # set condistions
-experiment.loc[3,1] = ['1M MeOH', '0.75M MeOH', '0.5M MeOH', '0.25M MeOH', '0.1M MeOH', '0mM MeOH']
-experiment.loc[3,2] = ['1M MeOH', '0.75M MeOH', '0.5M MeOH', '0.25M MeOH', '0.1M MeOH', '0mM MeOH']
-experiment.loc[3,3] = ['1M MeOH', '0.75M MeOH', '0.5M MeOH', '0.25M MeOH', '0.1M MeOH', '0mM MeOH']
-experiment.loc[3,4] = ['1M MeOH', '0.75M MeOH', '0.5M MeOH', '0.25M MeOH', '0.1M MeOH', '0mM MeOH']
-experiment.loc[3,5] = ['1M MeOH', '0.75M MeOH', '0.5M MeOH', '0.25M MeOH', '0.1M MeOH', '0mM MeOH']
-experiment.loc[3,6] = ['20mM serine', '15mM serine', '10mM serine', '5mM serine', '1mM serine', '0mM serine']
-experiment.loc[3,7] = ['20mM serine', '15mM serine', '10mM serine', '5mM serine', '1mM serine', '0mM serine']
-experiment.loc[3,8] = ['20mM serine', '15mM serine', '10mM serine', '5mM serine', '1mM serine', '0mM serine']
+experiment.loc[3,1] = ['condition 1', 'condition 2', 'condition 3']
+experiment.loc[3,2] = ['condition 1', 'condition 2', 'condition 3']
+experiment.loc[3,3] = ['condition 1', 'condition 2', 'condition 3']
+experiment.loc[3,4] = ['condition 1', 'condition 2', 'condition 3']
+experiment.loc[3,5] = ['condition 1', 'condition 2', 'condition 3']
+experiment.loc[3,6] = ['condition 1', 'condition 2', 'condition 3']
+experiment.loc[3,7] = ['condition 1', 'condition 2', 'condition 3']
+experiment.loc[3,8] = ['strain 1', 'strain 2', 'strain 3', 'strain 4', 'strain 5', 'strain 6']
+experiment.loc[3,9] = ['strain 1', 'strain 2', 'strain 3', 'strain 4', 'strain 5', 'strain 6']
+experiment.loc[3,10] = ['strain 1', 'strain 2', 'strain 3', 'strain 4', 'strain 5', 'strain 6']
 
-# set linestyles
+# set linestyles, preset 10 solid lines for all here
 linestyles = ['-','-','-','-','-','-','-','-','-','-']
 
-# set colors
+# set colors, preset 10 colors here
 cmap = ['k', 'b', 'r','g','c','m','y','saddlebrown','orange','olive']
 
-# calculations
+# calculations, this calculates doubling time ...
 DATA = g2plot.cal(DataFileName, inc_OD)
 
 # ploting
@@ -63,3 +85,14 @@ for column in experiment:
     g2plot.plot(DATA, experiment[column], WIN, TH, Figure_Type, Yscale, 
                 Xmax, Ymax, cmap, linestyles, savefig)
 plt.show()
+
+#%% 
+# view the whole plate, change nwells if not 96-well plate
+# Uncomment it if needed.
+# g2plot.viewPlate(DATA, nwells=96)
+
+#%% 
+# plot a list of wells
+# Uncomment it if needed.
+# Wells = ['A1', 'B1']  # wells to be plotted
+# g2plot.wellPlot(Wells, DATA)
