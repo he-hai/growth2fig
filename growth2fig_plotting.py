2f489420e749f92ff95d0567da08b4f655c7ff89
Hai He <He@mpimp-golm.mpg.de>
2019-07-26 13:33:05 +0200

    Added instructions and two new functions: viewplate and viewwells. Changed the default setting for plots. Ready to be pubulished.

--- growth2fig_plotting.py
+++ growth2fig_plotting.py
@@ -1,4 +1,12 @@
 # -*- coding: utf-8 -*-
+"""
+@Author: Hai
+The script works on Python >= 3.6 and requires pandas, matplotlib, numpy and scipy to work.
+The current verison of scripts will give you some warns, you may ignore them.
+The scipt works with the alonged script growth2fig_manager.py.
+Please use the manager and it is not necessary to modify this script for working. 
+"""
+
 import re
 import numpy as np
 import pandas as pd
@@ -70,9 +78,9 @@ def plot(data, MATS, WIN, TH, Figure_Type, Yscale, Xmax, Ymax, cmap, linestyles,
                                                 + str(biomassYield_sd) + ')'
         else:
             MATS.loc[3][MATS.loc[2].index(mat)] = MATS.loc[3][MATS.loc[2].index(mat)] + ': NG'
-
+    
     # ploting all/mean
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=(12,10))
     lines = []
     line_num = []
     
@@ -81,26 +89,66 @@ def plot(data, MATS, WIN, TH, Figure_Type, Yscale, Xmax, Ymax, cmap, linestyles,
         if Figure_Type == 'mean':
             line_num.append(len(lines))
             lines += ax.plot(data.index, data[mat].mean(axis=1),
-                             linestyle=linestyles[i], color=cmap[i], linewidth=2)        # label=MATS[3][i] 
+                             linestyle=linestyles[i], color=cmap[i], linewidth=3)    # label=MATS[3][i] 
         elif Figure_Type =='all':
             line_num.append(len(lines))
             lines += ax.plot(data.index, data[mat],label=MATS[3][i],
-                             linestyle=linestyles[i], color=cmap[i], linewidth=2)
+                             linestyle=linestyles[i], color=cmap[i], linewidth=3)
         
-    ax.grid(which='major', axis='both', linewidth=1, linestyle='-', color='0.75')
-    ax.grid(which='minor', axis='y', linewidth=0.75, linestyle='--', color='0.75')
+    ax.grid(which='major', axis='x', linewidth=1, linestyle='-', color='0.75')
+    ax.grid(which='major', axis='y', linewidth=1, linestyle='--', color='0.75')
     ax.tick_params(which='both',direction='in', bottom=True, right=False, 
-                   top=False, left=True)
+                   top=False, left=True, labelsize=14)
     ax.spines['right'].set_visible(False)
     ax.spines['top'].set_visible(False)
     ax.set_yscale(Yscale)
+
     if Yscale == 'log':
         ax.set_ylim(top=Ymax)
     elif Yscale == 'linear':
         ax.set_ylim(bottom=0,top=Ymax)
-    plt.setp(ax, title=MATS[1], ylabel='OD600', xlabel='Time (h)',
-         xlim=(0, Xmax))
-    ax.legend([lines[i] for i in line_num], MATS[3][:])
+    
+    ax.set_title(MATS[1], fontsize = 24)
+    ax.set_ylabel('OD600', fontsize = 18)
+    ax.set_xlabel('Time (h)', fontsize = 18)
+    ax.set_xlim(left=0, right=Xmax)
+    ax.legend([lines[i] for i in line_num], MATS[3][:], fontsize=16)
+    
     if savefig != 'no':
         title = get_valid_filename(MATS[1])
         fig.savefig('{}_{}.{}'.format(Figure_Type, title,savefig),dpi=300)
+
+def viewPlate(data, nwells=96):
+    """ Please confirm the type of plate,
+    96-well or others.
+    """
+    if nwells == 96:
+        nrows = 8
+        ncols = 12
+    elif nwells == 48:
+        nrows = 6
+        ncols = 8
+    row = ['A','B','C','D','E','F','G','H']
+    col = list(range(1,13))
+
+    fig, axes = plt.subplots(nrows=nrows,ncols=ncols,sharex=True,sharey=True)
+    fig.set_size_inches(15, 10)
+
+    for i in range(nrows):
+        for j in range(ncols):
+            well = row[i] + str(col[j])
+            if well in data.columns.values.tolist():
+                axes[i,j].plot(data.index, data.loc[:,well], linewidth=3)
+
+    plt.show()
+
+def wellPlot(Wells, data):
+    """Please enter wells in list, e.g. ['A1','A2',]"""
+    ID = data.columns.values.tolist()
+    plt.figure(figsize=(12, 10))
+    if np.all(np.isin(Wells, ID)):
+        plt.plot(data.index, data.loc[:,Wells], linewidth=3)
+        plt.legend(Wells)
+        plt.show()
+    else:
+        print("Please enter valid wells, e.g. ['A1','A2','A3']")
